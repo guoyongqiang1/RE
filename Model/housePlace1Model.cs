44932a24eecfecaf0f7357747af7d94a4eaeeef8
201819164225 <3505187760@qq.com>
2020-06-06 10:43:54 +0800

    测试

--- Model/housePlace1Model.cs
+++ Model/housePlace1Model.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 public class Class1
 {
-	public Class1()
+	public class housePlace1
 	{
-	}
+        public string Qu { get; set; }
+    public string Street { get; set; }
+      }
 }
